c87aff90ebb8ee357339b32b9f502eac2bfefaf2
oscar <oscaroceguera@gmail.com>
2020-01-02 21:36:32 -0700

    Added react-md and sass

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {NavigationDrawer} from 'react-md'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NavigationDrawer
+        toolbarTitle="react-md with create-react-app v2"
+        drawerTitle="react-app"
+      >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </NavigationDrawer>
   );
 }
 
